6561b9eed3c50c8cf2e21426cceac70cdadd3e71
abeforgit <arnebertrand@gmail.com>
2024-03-15 19:14:32 +0100

    initial commit

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,14 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     // Add options here
+    autoImport: {
+      webpack: {
+        resolve: { fallback: { vm: require.resolve('vm-browserify') } },
+      },
+    },
+    '@appuniversum/ember-appuniversum': {
+      disableWormholeElement: true,
+    },
   });
 
   return app.toTree();
